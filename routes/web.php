400a0f9dd531e4ada25a7f138aa26fc756c0fdc9
Marcelo Becker <marcelobecker@MacBook-Pro-de-Marcelo.local>
2017-12-27 22:07:55 -0200

    Criando controller para administrar usuarios

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,6 @@ Route::group([
         Route::get('dashboard', function(){
             return view('admin.dashboard');
         });
+        Route::resource('users', 'UsersController');
     });
 });
